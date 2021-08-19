b8aa781abb43db252735a4444ddc03062827d1b5
Oluwafemi Seun <51615334+SeunPaul@users.noreply.github.com>
2021-08-19 21:50:24 +0100

    This is a push of the completed resume page

---
+++ myName.js
@@ -0,0 +1,13 @@
+/* A Javascript Programs that prints my name */
+
+// my first name
+var myFirstName = "Seun"
+
+// my last name
+var myLastName = "Oluwafemi"
+
+// my full name
+var myFullName = `${myFirstName} ${myLastName}`
+
+// print full name to the console
+console.log(myFullName)
